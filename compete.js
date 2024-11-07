8a232be9f3dc345160ea8a8f6842d4fd97b4de96
mariusschueller <mariuseschueller@gmail.com>
2024-11-07 14:50:58 -0600

    initial frontend added

---
+++ compete.js
@@ -0,0 +1,5 @@
+function getSentence(){
+    document.getElementById('sentence-section').style.display = "block";
+    document.getElementById('get-sentence').style.display = "none";
+    document.getElementById('user-sentence-input').style.display = "block";
+}
---
+++ navigation.js
@@ -0,0 +1,10 @@
+function navigateToType() {
+    window.location.href = "type.html"; // Replace with your desired URL
+}
+
+function navigateToCompete() {
+    window.location.href = "compete.html"; // Replace with your desired URL
+}
+function navigateToHome() {
+    window.location.href = "index.html"; // Replace with your desired URL
+}
