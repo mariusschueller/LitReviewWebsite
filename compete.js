158d9c48b3fec1cec6f627471c1d9ac40b5620cc
mariusschueller <mariuseschueller@gmail.com>
2024-11-07 15:00:55 -0600

    compete page stuff added

--- compete.js
+++ compete.js
@@ -2,4 +2,8 @@ function getSentence(){
     document.getElementById('sentence-section').style.display = "block";
     document.getElementById('get-sentence').style.display = "none";
     document.getElementById('user-sentence-input').style.display = "block";
+    
+}
+function yearEntered(){
+    document.getElementById('computer-guess').style.display = "block";
 }
